9dc630e11b6158e3e70eec44cacab219352dc936
TynK-M <164543445+TynK-M@users.noreply.github.com>
2025-03-24 12:39:14 +0100

    Aggiunta Entity "acquisto"

---
+++ src/main/java/org/generationitaly/cinehub/entity/Acquisto.java
@@ -0,0 +1,39 @@
+package org.generationitaly.cinehub.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "acquisto")
+public class Acquisto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_utente", nullable = false)
+	private Utente utente;
+	@ManyToOne
+	@JoinColumn(name = "id_spettacolo", nullable = false)
+	private Spettacolo spettacolo;
+	@Column(name = "data_acquisto", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime dataAcquisto = LocalDateTime.now(); // valore di default
+	
+}
