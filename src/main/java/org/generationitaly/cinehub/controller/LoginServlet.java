873b9d7ab0d07bbbdb142e2e4832ba620b1f5ae6
Abdessamad G <99208738+1Abdess@users.noreply.github.com>
2025-03-25 09:51:27 +0100

    aggiunta hasing password con jBcrypt e implementazione della sua libreria sul pom.xml

--- src/main/java/org/generationitaly/cinehub/controller/LoginServlet.java
+++ src/main/java/org/generationitaly/cinehub/controller/LoginServlet.java
@@ -3,21 +3,25 @@ package org.generationitaly.cinehub.controller;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+
 import java.io.IOException;
-import org.generationitaly.cinehub.repository.UtenteRepository;
+
 import org.generationitaly.cinehub.entity.Utente;
+import org.generationitaly.cinehub.repository.UtenteRepository;
+import org.generationitaly.cinehub.Util.PasswordUtil;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
         UtenteRepository repo = new UtenteRepository();
-        Utente utente = repo.findByEmailAndPassword(email, password);
+        Utente utente = repo.findByEmail(email);
 
-        if (utente != null) {
+        if (utente != null && PasswordUtil.checkPassword(password, utente.getPassword())) {
             HttpSession session = request.getSession();
             session.setAttribute("utente", utente);
             response.sendRedirect("home.jsp");
--- src/main/java/org/generationitaly/cinehub/controller/RegistrazioneServlet.java
+++ src/main/java/org/generationitaly/cinehub/controller/RegistrazioneServlet.java
@@ -3,22 +3,28 @@ package org.generationitaly.cinehub.controller;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+
 import java.io.IOException;
+
 import org.generationitaly.cinehub.entity.Utente;
 import org.generationitaly.cinehub.repository.UtenteRepository;
+import org.generationitaly.cinehub.Util.PasswordUtil;
 
 @WebServlet("/RegistrazioneServlet")
 public class RegistrazioneServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String nome = request.getParameter("nome");
+        String cognome = request.getParameter("cognome");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
         Utente utente = new Utente();
         utente.setNome(nome);
+        utente.setCognome(cognome);
         utente.setEmail(email);
-        utente.setPassword(password);
+        utente.setPassword(PasswordUtil.hashPassword(password)); // <-- HASH
 
         UtenteRepository repo = new UtenteRepository();
         repo.save(utente);
